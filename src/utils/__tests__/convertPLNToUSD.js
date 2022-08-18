d1f3ad2ab16d14bd2882a786606796bfeef18ac9
Ada Jasik <jollyzombie@protonmail.com>
2022-08-18 16:14:22 +0200

    Currency Form Unit tests

---
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -0,0 +1,28 @@
+import { convertPLNToUSD } from './../convertPLNtoUSD';
+
+describe('ConvertPLNtoUSD', () => {
+  it('should return proper value when good input', () => {
+    expect(convertPLNToUSD(1)).toBe('$0.29');
+    expect(convertPLNToUSD(2)).toBe('$0.57');
+    expect(convertPLNToUSD(20)).toBe('$5.71');
+    expect(convertPLNToUSD(12)).toBe('$3.43');
+  });
+  it('should return Nan when text input', () => {
+    expect(convertPLNToUSD('3')).toBeNaN();
+    expect(convertPLNToUSD('kje8')).toBeNaN();
+    expect(convertPLNToUSD('P.%^/')).toBeNaN();
+  });
+  it('should return NaN when input is empty', () => {
+    expect(convertPLNToUSD()).toBeNaN();
+  });
+  it('should return error when input is different than number and string', () => {
+    expect(convertPLNToUSD(null)).toBe('Error');
+    expect(convertPLNToUSD(function(){})).toBe('Error');
+    expect(convertPLNToUSD({})).toBe('Error');
+  });
+  it('should return error when input is lower that zero', () => {
+    expect(convertPLNToUSD(-55)).toBe('$0.00');
+    expect(convertPLNToUSD(0)).toBe('$0.00');
+    expect(convertPLNToUSD(-7)).toBe('$0.00');
+  });
+});
